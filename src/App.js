9b931d5ed9cb3c502705be266e8477718268c3dc
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-12 12:07:16 +0545

    fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header name={name} />
-      <About />gi
+      <About />
       <Table tableInfo={tableInfo} languages={languages} />
     </div>
   );
